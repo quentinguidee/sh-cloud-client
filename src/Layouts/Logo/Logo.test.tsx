440a82d3b124086283aeae6cac692cd7239fdd23
Quentin Guidée <quentin.guidee@gmail.com>
2022-08-03 13:21:32 +0200

    Fix Logo unit tests
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/Layouts/Logo/Logo.test.tsx
+++ src/Layouts/Logo/Logo.test.tsx
@@ -3,8 +3,14 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 
 import Logo from "./Logo";
+import { Provider } from "react-redux";
+import store from "Store/Store";
 
 it("renders", async () => {
-    const { getByText } = render(<Logo />);
+    const { getByText } = render(
+        <Provider store={store}>
+            <Logo />
+        </Provider>,
+    );
     expect(getByText("cloud.sh")).toBeInTheDocument();
 });
