6775979848cba9037a9c31869a089bfdfc9e20a0
Quentin Guidée <quentin.guidee@gmail.com>
2022-08-03 21:13:47 +0200

    Fix /admin/database with SQLite
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/Pages/Admin/Categories/AdminDatabase.tsx
+++ src/Pages/Admin/Categories/AdminDatabase.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import TitleBar from "Layouts/TitleBar/TitleBar";
 import axios from "axios";
 import { useToken } from "Store/Hooks/useToken";
@@ -9,6 +9,7 @@ import Layout from "Components/Layout/Layout";
 import { Text } from "Components/Text/Text";
 
 type Database = {
+    dbms?: string;
     host?: string;
     name?: string;
     user?: string;
@@ -33,28 +34,19 @@ function AdminDatabase() {
 
     useEffect(() => load(), []);
 
-    const { host, name, user, password } = database;
-
     let component;
-    if (host === undefined) {
+    if (database?.dbms === undefined) {
         component = <Text>Loading...</Text>;
     } else {
-        component = (
-            <Fragment>
-                <InlineInfo title="host">
-                    <Code>{host}</Code>
-                </InlineInfo>
-                <InlineInfo title="name">
-                    <Code>{name}</Code>
-                </InlineInfo>
-                <InlineInfo title="user">
-                    <Code>{user}</Code>
-                </InlineInfo>
-                <InlineInfo title="password">
-                    <Code>{password}</Code>
-                </InlineInfo>
-            </Fragment>
-        );
+        component = [];
+        for (const key in database) {
+            const value = database[key];
+            component.push(
+                <InlineInfo title={key}>
+                    <Code>{value}</Code>
+                </InlineInfo>,
+            );
+        }
     }
 
     return (
