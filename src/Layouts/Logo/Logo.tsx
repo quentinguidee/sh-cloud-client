a2882964bd7c8f817ac94ec39c2ff72fd37f6666
Quentin Guidée <quentin.guidee@gmail.com>
2022-08-03 13:18:11 +0200

    Add DarkMode app logo
    Signed-off-by: Quentin Guidée <quentin.guidee@gmail.com>

--- src/Layouts/Logo/Logo.tsx
+++ src/Layouts/Logo/Logo.tsx
@@ -1,11 +1,13 @@
 import classNames from "classnames";
 import React from "react";
 
-import LogoPNG from "Assets/Images/logo.png";
+import LogoLightModePNG from "Assets/Images/LogoLightMode.png";
+import LogoDarkModePNG from "Assets/Images/LogoDarkMode.png";
 
 import styles from "./Logo.module.sass";
 import { Text } from "Components/Text/Text";
 import Layout from "Components/Layout/Layout";
+import { useTheme } from "Store/Hooks/useTheme";
 
 type Props = React.HTMLProps<HTMLDivElement> & {
     // Small hides the text
@@ -14,6 +16,9 @@ type Props = React.HTMLProps<HTMLDivElement> & {
 
 function Logo(props: Props) {
     const { className, small, ...others } = props;
+
+    const theme = useTheme();
+
     return (
         <Layout
             horizontal
@@ -23,7 +28,15 @@ function Logo(props: Props) {
             {...others}
             className={classNames(styles.logo, className)}
         >
-            <img alt="Logo" src={LogoPNG} className={styles.image} />
+            <img
+                alt="Logo"
+                src={
+                    theme.current === "light"
+                        ? LogoLightModePNG
+                        : LogoDarkModePNG
+                }
+                className={styles.image}
+            />
             {!small && <Text>cloud.sh</Text>}
         </Layout>
     );
